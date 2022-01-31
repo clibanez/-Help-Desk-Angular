ec727ed5a7462777486c2ad876a616ec7556e782
Clibanez <clibanezzcaldas@gmail.com>
2022-01-31 14:17:19 -0300

    HTML e CSS para compenent tecnico-list

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from '../../../model/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+    ELEMENT_DATA:Tecnico[] = [
+      {
+        id:1,
+        nome:'Clibanez Caldas',
+        cpf:'061.234.104-66,',
+        email:'test@gmai.com',
+        senha:'123',
+        perfis: ['0'],
+        dataCriacao:'15/08/2022'
+
+
+      }
+    ]
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
