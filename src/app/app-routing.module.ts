cea5f86beb236c456de018ddc781bb9b7c6846c0
Clibanez <clibanezzcaldas@gmail.com>
2022-02-14 12:47:14 -0300

    Componente Tecnico-Update Implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
@@ -8,13 +9,15 @@ import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 
+
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: '' , component: NavComponent, canActivate: [AuthGuard], children: [
       { path: 'home', component: HomeComponent },
       { path: 'tecnicos', component: TecnicoListComponent},
-      {path: 'tecnicos/create', component: TecnicoCreateComponent}
+      {path: 'tecnicos/create', component: TecnicoCreateComponent},
+      {path: 'tecnicos/update/:id', component: TecnicoUpdateComponent}
 
     ]
   }
